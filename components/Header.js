1923002ad1bc1deaa0c51a1939ff3c0d1f360083
Ruslana Semenenko <katotochkaaa@gmail.com>
2023-12-28 23:04:42 +0000

    work

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, } from "react-native";
 export default function Header() {
     return (
       <View style={styles.main}>
-        <Text style={styles.text}> Checklist </Text>
+        <Text style={styles.text}>Checklist</Text>
       </View>
     );
 }
